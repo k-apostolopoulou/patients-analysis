f21914440e7adc4ae55d0418a7f631617c5a472e
Konstantina Apostolopoulou <k.apostolopoulou@hua.gr>
2024-02-21 16:09:30 +0200

    add docstring

--- calculator_mean.py
+++ calculator_mean.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 def calculate_mean(data):
+"""This function computes the mean of an array.
+Input:A one dimensional arra
+Output: The mean of the array"""
 	mean=np.mean(data)
 	return mena
